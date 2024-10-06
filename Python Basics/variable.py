b1800d51bea9d0f2535753265b114dff98581f89
coderzaman <coder.aktar@gmail.com>
2024-10-07 00:12:58 +0600

    Start Python Journey

---
+++ Python Basics/variable.py
@@ -0,0 +1,13 @@
+# Variable storing an integer
+x = 5
+print(x);
+
+# Variable storing a string
+name = "Alice"
+print(name)
+# Variable storing a floating-point number
+pi = 3.14159
+print(pi)
+# Variable storing a list
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
